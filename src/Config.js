95281be61b6e4d9595497a67d9c499ea2de888a5
GlennFolker <glenn.folk08@gmail.com>
2020-09-25 14:07:10 +0700

    what the heck is wrong with .gitignore

--- src/Config.js
+++ src/Config.js
@@ -2,7 +2,7 @@
 
 class Config{
 	static prefix = "sn!";
-	static token = "NzQ2MDA1ODY0MzYzNzIwNzk2.Xz6CGg.C8Lm5RJNjjIzyGwUnIxXHPQ16gY";
+	static token = "NzQ2MDA1ODY0MzYzNzIwNzk2.Xz6CGg.iQX78zx-hh6aR9Zv53k9V-40rDk";
 };
 
 globalThis.Config = Config;
